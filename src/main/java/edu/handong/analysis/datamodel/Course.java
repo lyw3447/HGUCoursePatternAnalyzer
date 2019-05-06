f70fc16b8601a434d5355c0456a9816ccebc2b9b
lyw3447 <43979679+lyw3447@users.noreply.github.com>
2019-05-06 21:23:26 +0900

    Add files via upload

---
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -0,0 +1,14 @@
+package edu.handong.analysis;
+
+public class Course {
+	private String courseName;
+
+	public Course(String name) {
+		courseName = name;
+	}
+	
+	public String getCourseName() {
+		return courseName;
+	}
+
+}
---
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -0,0 +1,13 @@
+package edu.handong.analysis;
+
+public class Student {
+	private String name;
+	
+	public Student(String name) {
+		name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
